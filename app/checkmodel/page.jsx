86941df9286be5cc509f69691944c9fe5b1deb3e
harycoder1 <harish7823@gmail.com>
2023-10-10 09:32:34 +0530

    initial commit 18 for events try to solve bugs , adding the fetch only data

--- app/checkmodel/page.jsx
+++ app/checkmodel/page.jsx
@@ -13,6 +13,13 @@ function MyPage() {
     setIsModalOpen(false);
   };
 
+  const Data = async()=>{
+  const res =await fetch('api/committee');
+    const data = await res.json()
+    console.log(data)
+  }
+  
+  Data();
   return (
     <div>
       <button onClick={openModal} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
